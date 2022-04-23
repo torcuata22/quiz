73e0bdd3e118633f08d09b437eb0f18c44783e5d
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-04-23 15:08:16 -0400

    creating setting up questions and answers

--- quiz/src/components/QuestionCard.tsx
+++ quiz/src/components/QuestionCard.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
-const QuestionCard = () => <div> QuestionCard</div>;
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: boolean;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question, 
+    answers, 
+    callback, 
+    userAnswer, 
+    questionNr,
+    totalQuestions,
+}) => ( 
+    <div> 
+        <p className="number">
+            Question: {questionNr} / {totalQuestions}
+
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question}}></p>
+      <div>
+        {answers.map(answer => (
+            <div>
+                <button disabled={userAnswer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{ __html: answer}} />
+                </button>
+            </div>
+        ))}
+      </div>
+    
+    
+    
+    QuestionCard</div> 
+);
 
 export default QuestionCard;
