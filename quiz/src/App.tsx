77cf2b6b583e15c2866b2db54202f7b8a894c7c5
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-04-23 16:47:30 -0400

    QuestionCard

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,32 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import {fetchQuizQuestions} from './API';
 //components:
 import QuestionCard from './components/QuestionCard'
-function App() {
+//types:
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
+const App = () =>   {
+  const [loading, setloading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setStcore] = useState(0)
+  const [gameOver, setGameOver] = useState(true);
+
+  //console.log(questions);
+
+
   
   const startTrivia = async () => {
+    setloading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
 
-  } 
+    setQuestions(newQuestions);
+    setStcore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setloading(false);
+
+  }; 
   
   const checkAnswer = (e: React.MouseEvent <HTMLButtonElement>) => {
 
   }
 
-  const nextQuestion = () => {
-
-  }
-  
-  
+  const nextQuestion = () => {};
   return (
     <div className="App">
          <h1>Book Trivia</h1>
-         <button className="start" onClick={startTrivia}>
-          Start
-         </button>
-         <p className="score"> Score: </p>
-         <p>Loading next question...</p>
-         <QuestionCard/>
+         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+              Start
+          </button>
+         ) : null}
+         {!gameOver ? <p className="score"> Score: </p> : null}
+         {loading && <p>next question...</p>}
+         <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+  callback={checkAnswer} />
+         
          <p></p>
          <button className="next" onClick={nextQuestion}>Next Question</button>
 
--- quiz/src/components/utils.ts
+++ quiz/src/components/utils.ts
@@ -1,1 +1,3 @@
 import React from 'react';
+export const shuffleArray = (array: any[]) =>
+    [... array].sort(()=> Math.random() -0.5);
