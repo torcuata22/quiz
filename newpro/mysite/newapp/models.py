6bfc6ee19204799778f1a1eddc8575c092208c62
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-01 13:52:19 -0500

    dummy data

--- newpro/mysite/newapp/models.py
+++ newpro/mysite/newapp/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Movies(models.Model):
+    
     name = models.CharField(max_length=200)
     rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
